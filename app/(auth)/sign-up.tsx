aa62531955cb2a429e88a0517d92161da13fa49d
asyncasad <asyncasad@gmail.com>
2025-06-12 22:14:29 +0530

    feat(input): add input fields on signup page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,20 +1,51 @@
-import { images } from "@/constants";
-import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import React, { useState } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+  });
+
   return (
-    <ScrollView className="flex-1 bg-white">
-      <View className="flex-1 bg-white">
-        <View className="relative w-full h-[250px]">
-          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
-            Create your account
-          </Text>
-          <View className="p-5"></View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      className="flex-1"
+    >
+      <ScrollView
+        className="flex-1 bg-white"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View className="flex-1 bg-white">
+          <View className="relative w-full h-[250px]">
+            <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+            <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+              Create your account
+            </Text>
+          </View>
+          <View className="p-5 flex flex-col gap-5">
+            <InputField
+              label="Name"
+              placeholder="Enter your name"
+              icon={icons.person}
+              value={form.name}
+              onChangeText={(value: string) =>
+                setForm({ ...form, name: value })
+              }
+            />
+          </View>
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
