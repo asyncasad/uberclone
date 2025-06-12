a73de0069f902371b528f2ce2021c3abbff8b9e1
Asadkhan Pathan <asyncasad@Asadkhans-MacBook-Pro.local>
2025-06-12 15:30:15 +0530

    feat(project): configure project structure

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+const Chat = () => {
+  return (
+    <SafeAreaView>
+      <Text>Chat</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Chat;
+
+const styles = StyleSheet.create({});
---
+++ app/(root)/(tabs)/history.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
+
+const History = () => {
+  return (
+    <SafeAreaView>
+      <Text>History</Text>
+    </SafeAreaView>
+  );
+};
+
+export default History;
+
+const styles = StyleSheet.create({});
