c400be6f7075599fffd3ac463559953a6e433276
Asadkhan Pathan <asyncasad@Asadkhans-MacBook-Pro.local>
2025-06-12 19:04:12 +0530

    feat(onboarding): add onboarding screens

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,20 @@
+import { images } from "@/constants";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create your account
+          </Text>
+          <View className="p-5"></View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
